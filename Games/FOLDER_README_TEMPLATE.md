93f8c4db9cfe424b7e30f7b060b2dc1fdf00c22a
aasn0119 <abhitrivedi119@gmail.com>
2023-05-20 22:34:21 +0530

    made tower_of_hanoi game

--- Games/FOLDER_README_TEMPLATE.md
+++ Games/FOLDER_README_TEMPLATE.md
@@ -1,21 +1,46 @@
-# **Game_Name** 
+# **Tower Of Hanoi** 
 
 ---
 
 <br>
 
 ## **Description 📃**
-<!-- add your game description here  -->
-- 
+
+- The goal of the Tower of Hanoi game is to move all the disks from the starting peg to the target peg, using the auxiliary peg as needed.
+
+- The game is considered successful when all the disks are moved to the target peg, preserving their original order.
+
+- The game is made using HTML5, CSS3, SCSS, JavaScript.
 
 ## **functionalities 🎮**
-<!-- add functionalities over here -->
-- 
+
+- Disk Movement:
+
+> Only one disk can be moved at a time.
+> A disk can be moved from the top of one peg to the top of another peg.
+> It is not allowed to place a larger disk on top of a smaller disk at any point during the game.
+
 <br>
 
 ## **How to play? 🕹️**
 <!-- add the steps how to play games -->
-- 
+-  Pegs:
+
+> The game consists of three pegs: the starting peg, the target peg, and the auxiliary peg.
+> The disks are initially placed on the starting peg.
+> The target peg is the final destination where all the disks should be moved.
+> The auxiliary peg can be used as an intermediate peg to facilitate the movement of disks.
+> You can move the disks using mouse and left and right mouse buttons.
+
+- Minimum Moves:
+
+The minimum number of moves required to solve the Tower of Hanoi puzzle with n disks is 2^n - 1.
+This means that for each additional disk, the number of moves required doubles and then adds one.
+
+-Complexity and Difficulty:
+
+The Tower of Hanoi puzzle becomes more challenging as the number of disks increases.
+Solving the puzzle with a large number of disks can require a significant amount of time and logical thinking
 
 <br>
 
@@ -23,7 +48,7 @@
 
 <br>
 <!-- add your screenshots like this -->
-<!-- ![image](url) -->
+![image](../assets/images/Tower_Of_Hanoi.jpg) 
 
 <br>
 
---
+++ Games/Tower_Of_Hanoi/app.js
@@ -0,0 +1,108 @@
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function blank(e) {
+    let model = document.querySelector('.model');
+    model.style.display = 'none';
+})
+
+
+
+$(document).ready(function() {
+    var towers = [[[], $(".line1")], [[], $(".line2")], [[], $(".line3")]],
+      moves = 0,
+      discs = null,
+      hold = null;
+    function clear() {
+      towers[0][1].empty();
+      towers[1][1].empty();
+      towers[2][1].empty();
+    }
+  
+    function drawdiscs() {
+      clear();
+      for (var i = 0; i < 3; i++) {
+        if (!jQuery.isEmptyObject(towers[i][0])) {
+          for (var j = 0; j < towers[i][0].length; j++) {
+            towers[i][1].append(
+              $(
+                "<li id='disc" +
+                  towers[i][0][j] +
+                  "' value='" +
+                  towers[i][0][j] +
+                  "'></li>"
+              )
+            );
+          }
+        }
+      }
+    }
+  
+    function init() {
+      clear();
+      towers = [[[], $(".line1")], [[], $(".line2")], [[], $(".line3")]];
+      discs = document.getElementById("box").value;
+      moves = 0;
+      hold = null;
+      for (var i = discs; i > 0; i--) towers[0][0].push(i);
+      drawdiscs();
+      $(".moves").text(moves + " moves");
+    }
+  
+    function handle(tower) {
+      if (hold === null) {
+        if (!jQuery.isEmptyObject(towers[tower][0])) {
+          hold = tower;
+          towers[hold][1]
+            .children()
+            .last()
+            .css("margin-top", "-190px");
+        }
+      } else {
+        var move = moveDisc(hold, tower);
+        moves += 1;
+        $(".moves").text(moves + " moves");
+        if (move == 1) {
+          drawdiscs();
+        } else {
+          alert("You can't place a bigger disc on a smaller one");
+        }
+        hold = null;
+      }
+      if (solved()) alert("Solved with " + moves + " moves!");
+    }
+  
+    function moveDisc(a, b) {
+      var from = towers[a][0];
+      var to = towers[b][0];
+      if (from.length === 0) return 0;
+      else if (to.length === 0) {
+        to.push(from.pop());
+        return 1;
+      } else if (from[from.length - 1] > to[to.length - 1]) {
+        return 0;
+      } else {
+        to.push(from.pop());
+        return 1;
+      }
+    }
+  
+    function solved() {
+      if (
+        jQuery.isEmptyObject(towers[0][0]) &&
+        jQuery.isEmptyObject(towers[1][0]) &&
+        towers[2][0].length == discs
+      )
+        return 1;
+      else 
+        return 0;
+    }
+  
+    $(".t").click(function() {
+      handle($(this).attr("value"));
+    });
+  
+    $("#restart").click(function() {
+      var discs = document.getElementById("box").value;
+      init();
+    });
+    init();
+  });

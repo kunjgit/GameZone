fb6c483a1110acedc79afead2617d6e35124cb09
Akhil <akhilenderb9@gmail.com>
2023-05-22 17:44:33 +0530

    Added comments

--- Games/Whack_A_Mole/script.js
+++ Games/Whack_A_Mole/script.js
@@ -10,16 +10,19 @@ window.onload = function() {
 function setGame() {
 
     for (let i = 0; i < 9; i++) { 
+        //i goes from 0 to 8, stops at 9
+        //<div id="0-8"></div>
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 500); 
-    setInterval(setPlant, 1000); 
+    setInterval(setMole, 800); // 800 miliseconds = 0.8 second, every 0.8 second call setMole
+    setInterval(setPlant, 1000); // 1000 miliseconds = 1 seconds, every 1 second call setPlant
 }
 
 function getRandomTile() {
+     //math.random --> 0-1 --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
@@ -66,10 +69,10 @@ function selectTile() {
     }
     if (this == currMoleTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString(); 
+        document.getElementById("score").innerText = score.toString(); //updating score
     }
     else if (this == currPlantTile) {
-        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); ////update game over
         gameOver = true;
     }
 }

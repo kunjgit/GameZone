4fd5d9dd0d4a83a9719ab7977c79ed0b00b41e62
Akhil <akhilenderb9@gmail.com>
2023-05-22 16:57:30 +0530

    Completed making Whack A Mole Game

---
+++ Games/Whack_A_Mole/Readme.md
@@ -0,0 +1,32 @@
+# **Whack_A_Mole** 
+
+---
+
+<br>
+
+## **Description 📃**
+<!-- add your game description here  -->
+- this game is developed by html,css and js tech stacks and you can play it easily by using your mouse
+
+## **functionalities 🎮**
+<!-- add functionalities over here -->
+- there is mole and insectivorous plant popping out from the holes
+- there are 9 holes where they can pop out 
+<br>
+
+## **How to play? 🕹️**
+<!-- add the steps how to play games -->
+- You have to tap only on the mole
+- If you tap the insectivorous plant then the game is over
+- Just concentrate on the mole to score more points.
+<br>
+
+## **Screenshots 📸**
+
+<br>
+<!-- add your screenshots like this -->
+![image]("../../../../assets/images/Whack_A_Mole/Whack_A_Mole.png");
+<br>
+
+## **Working video 📹**
+<!-- add your working video over here -->
---
+++ Games/Whack_A_Mole/script.js
@@ -0,0 +1,75 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+
+    for (let i = 0; i < 9; i++) { 
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setMole, 500); 
+    setInterval(setPlant, 1000); 
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    if (gameOver) {
+        return;
+    }
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+    let mole = document.createElement("img");
+    mole.src ="../../assets/images/Whack_A_Mole/image2.png";
+
+    let num = getRandomTile();
+    if (currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "../../assets/images/Whack_A_Mole/image4.png";
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+    if (this == currMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString(); 
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameOver = true;
+    }
+}

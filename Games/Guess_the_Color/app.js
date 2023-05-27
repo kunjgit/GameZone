3cd4f9370d480df5c270a1f525f34b0e1da4838a
Vidhi Makani <vidhimakani007@gmail.com>
2023-05-27 18:14:00 +0530

    complete

--- Games/Guess_the_Color/app.js
+++ Games/Guess_the_Color/app.js
@@ -6,11 +6,49 @@ let b5 = document.getElementById('b5');
 let b6 = document.getElementById('b6');
 let rgb = document.getElementById('rgb');
 let box = document.getElementsByClassName('box');
+var easy = document.querySelector("#easy");
+var hard = document.querySelector("#hard");
+// var pickedColor = pickColor();
+
 
 let arr = ["b1", "b2", "b3", "b4", "b5", "b6"];
 var audio = new Audio("Assets/click.wav");
 var win = new Audio("Assets/win.mp3");
 var no = new Audio("Assets/no.mp3");
+
+// easy.addEventListener("click",function(){
+// 	easy.classList.add("selected");
+// 	hard.classList.remove("selected");
+// 	ns=3;
+// 	colors = randomColor(ns);
+// 	pickedColor = pickColor();
+// 	rgb.textContent = pickedColor;
+// 	for (var i = 0; i < squares.length; i++) {
+// 		if(colors[i]){
+// 			squares[i].style.background = colors[i];
+// 		}else{
+// 			squares[i].style.display = "none";
+// 		}
+// 	}
+// })
+
+// hard.addEventListener("click",function(){
+// 	hard.classList.add("selected");
+// 	easy.classList.remove("selected");
+// 	ns=6;
+// 	colors = randomColor(ns);
+// 	pickedColor = pickColor();
+// 	rgb.textContent = pickedColor;
+// 	for (var i = 0; i < squares.length; i++) {
+// 		squares[i].style.background = colors[i];
+// 		squares[i].style.display = "block";
+// 	}
+// })
+// colorDisplay.textContent = pickedColor;
+// function pickColor(){
+// 	var random = Math.floor(Math.random() * color.length);
+// 	return colors[random];
+// };
 
 //Genrate Random color
 function randomColor() {

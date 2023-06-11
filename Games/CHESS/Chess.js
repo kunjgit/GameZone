3ce40348cea27ea0efa6baa847c26f979306aebb
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-06-11 15:09:08 +0530

    requested changes done

--- Games/CHESS/Chess.js
+++ Games/CHESS/Chess.js
@@ -5,14 +5,14 @@ function insertImage() {
 
         if (image.innerText.length !== 0) {
             if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
-                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="${image.innerText}.png" alt="">`
+                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="../../assets/images/${image.innerText}.png" alt="">`
                 image.style.cursor = 'pointer'
 
             }
 
             else {
 
-                image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.png" alt="">`
+                image.innerHTML = `${image.innerText} <img class='allimg' src="../../assets/images/${image.innerText}.png" alt="">`
                 image.style.cursor = 'pointer'
             }
         }

6f9301a1cfe5aa9efd918f1153bf7d35c0953aec
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2023-06-20 15:11:20 +0530

    dino game chrome added

---
+++ Games/Dino_game/README.md
@@ -0,0 +1,23 @@
+# The Dinosaur Game
+
+![Game Screenshot](screenshots/ss1.png)
+
+## Description
+The Dinosaur Game is a simple and addictive browser-based game. It's an endless runner game where you control a T-Rex and try to avoid cacti by jumping over them. The longer you survive, the higher your score!
+
+## How to Play
+1. Open the game in a web browser.
+2. Press the spacebar or click anywhere on the screen to make the T-Rex jump.
+3. Time your jumps to avoid colliding with the cacti.
+4. The game will progressively get faster, making it more challenging to survive.
+5. Your score increases as you successfully jump over the cacti.
+6. The game ends when the T-Rex collides with a cactus.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Screenshots
+![Game Over Screenshot](screenshots/ss2.png)
+
---
+++ Games/Dino_game/script.js
@@ -0,0 +1,59 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
+
+
+//function for score
+let scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+
+//start Game
+window.addEventListener("keydown", (start) => {
+    //    console.log(start);
+    if (start.code == "Space") {
+        gameOver.style.display = "none";
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+
+        //score
+        let playerScore = 0;
+        interval = setInterval(scoreCounter, 200);
+    }
+});
+
+
+//jump Your Character
+window.addEventListener("keydown", (e) => {
+    if (e.key == "ArrowUp")
+        if (dino.classList != "dinoActive") {
+            dino.classList.add("dinoActive");
+            setTimeout(() => {
+                dino.classList.remove("dinoActive");
+            }, 500);
+        }
+});
+
+let result = setInterval(() => {
+    let dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
+    if (dinoBottom <= 90 && blockLeft >= 20 && blockLeft <= 145) {
+        gameOver.style.display = "block";
+        block.classList.remove("blockActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
+    }
+}, 10);

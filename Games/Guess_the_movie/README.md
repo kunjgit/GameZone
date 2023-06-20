54ebeabd439f558575d98eeea1c8ba010521447c
PR1YAM9 <priyam9maini@gmail.com>
2023-06-20 20:40:00 +0530

    Game Added

---
+++ Games/Guess_the_movie/README.md
@@ -0,0 +1,31 @@
+# **Guess the movie by emojis** 
+
+---
+
+<br>
+
+## **Description 📃**
+In this addictive game, your task is to decipher a secret word by carefully examining a sequence of emojis. These expressive pictographs hold the key to unlocking the hidden word, but it's up to you to unravel their cryptic messages. From common objects to famous places, movie titles, and even idioms, the emojis will encompass a wide range of subjects, ensuring an exciting and diverse gameplay experience.
+
+
+- 
+
+## **functionalities 🎮**
+Mouse Clicks
+- 
+<br>
+
+## **How to play? 🕹️**
+A series of emojis will be displayed on the screen, representing a secret word.
+Analyze the emojis and try to identify their individual meanings.
+Combine the meanings to form a word that accurately represents the emojis.
+Type your guess into the text box and submit it.
+- 
+
+<br>
+
+## **Screenshots 📸**
+![Alt text](image.png)
+<br>
+<!-- add your screenshots like this -->
+<!-- ![image](url) -->
---
+++ Games/Guess_the_movie/script.js
@@ -0,0 +1,129 @@
+const moviesObject = {
+    "👸👹🌹": "Beauty And The Beast",
+    "🧙👓⚡": "Harry Potter",
+    "🦇🃏 ": "Joker",
+    "👩‍❤️‍👨🚢🥶": "Titanic",
+    "👻👻🔫": "Ghostbusters",
+    "🐜👨 ": "Antman",
+    "🐀👨‍🍳 ": "Ratatouille",
+    "🖊️📓👩‍❤️‍👨": "The notebook",
+    "😈👗👠": "Devil Wears Prada",
+    "📱🍎 ": "Jobs",
+    "🎉🍺🎊❎": "This Is the End",
+    "🔍🐟 ": "Finding Nemo",
+    "👸📔 ": "Princess Diaries",
+    "🐔🏃 ": "Chicken Run",
+    "🙍‍♂️🦇": "Batman",
+    "🏠💐": "Housefull",
+    "💗💓✌️": "Dil Dhadakne Do",
+    "🕊️🤚🅱️":"Udta Punjab",
+    "🚶🚶":"Chalte Chalte"
+  };
+  const container = document.querySelector(".container");
+  const controls = document.querySelector(".controls-container");
+  const startButton = document.getElementById("start");
+  const letterContainer = document.getElementById("letter-container");
+  const userInputSection = document.getElementById("userInputSection");
+  const resultText = document.getElementById("result");
+  const hints = Object.keys(moviesObject);
+  let randomHint = "",
+    randomWord = "";
+  let winCount = 0,
+    lossCount = 5;
+  
+  const generateRandomValue = (array) => Math.floor(Math.random() * array.length);
+  
+  //Blocker
+  const blocker = () => {
+    let letterButtons = document.querySelectorAll(".letters");
+    letterButtons.forEach((button) => {
+      button.disabled = true;
+    });
+    stopGame();
+  };
+  
+  //Start game
+  startButton.addEventListener("click", () => {
+    //Controls and buttons visibility
+    controls.classList.add("hide");
+    init();
+  });
+  
+  //Stop Game
+  const stopGame = () => {
+    controls.classList.remove("hide");
+  };
+  
+  //Generate Word
+  const generateWord = () => {
+    letterContainer.classList.remove("hide");
+    userInputSection.innerText = "";
+    randomHint = hints[generateRandomValue(hints)];
+    randomWord = moviesObject[randomHint];
+    container.innerHTML = `<div id="movieHint">${randomHint}</div>`;
+    let displayItem = "";
+    randomWord.split("").forEach((value) => {
+      if (value == " ") {
+        winCount += 1;
+        displayItem += `<span class="inputSpace">&nbsp;</span>`;
+      } else {
+        displayItem += `<span class="inputSpace">_</span>`;
+      }
+    });
+    userInputSection.innerHTML = displayItem;
+  };
+  
+  //Initial Function
+  const init = () => {
+    winCount = 0;
+    lossCount = 5;
+    document.getElementById(
+      "chanceCount"
+    ).innerHTML = `<span>Tries Left:</span>${lossCount}`;
+    randomHint = null;
+    randomWord = "";
+    userInputSection.innerHTML = "";
+    letterContainer.classList.add("hide");
+    letterContainer.innerHTML = "";
+    generateWord();
+    for (let i = 65; i < 91; i++) {
+      let button = document.createElement("button");
+      button.classList.add("letters");
+      //Number to ASCII [A - Z]
+      button.innerText = String.fromCharCode(i);
+      //Character button click
+      button.addEventListener("click", () => {
+        let charArray = randomWord.toUpperCase().split("");
+        let inputSpace = document.getElementsByClassName("inputSpace");
+        if (charArray.includes(button.innerText)) {
+          charArray.forEach((char, index) => {
+            if (char === button.innerText) {
+              button.classList.add("used");
+              inputSpace[index].innerText = char;
+              winCount += 1;
+              if (winCount == charArray.length) {
+                resultText.innerHTML = "You Won";
+                blocker();
+              }
+            }
+          });
+        } else {
+          lossCount -= 1;
+          document.getElementById(
+            "chanceCount"
+          ).innerHTML = `<span>Tries Left:</span> ${lossCount}`;
+          button.classList.add("used");
+          if (lossCount == 0) {
+            resultText.innerHTML = "Game Over";
+            blocker();
+          }
+        }
+        button.disabled = true;
+      });
+      letterContainer.appendChild(button);
+    }
+  };
+  
+  window.onload = () => {
+    init();
+  };

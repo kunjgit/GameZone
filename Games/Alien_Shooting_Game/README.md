7d7b3f77c49d3f74170c3b800e6ce2e6da4ffa51
Sujan Ghosh <sujanrupu@gmail.com>
2023-06-23 03:11:08 +0530

    Added Alien_Shooting_Game(Issue: #1867)

---
+++ Games/Alien_Shooting_Game/README.md
@@ -0,0 +1,25 @@
+# **Game_Name** 
+
+Alien Shooting Game
+
+## **Description 📃**
+Added Allien Shooting Game developed using frontend technologies, HTML, CSS, JavaScript as contributor **GSSOC '23**
+
+## **functionalities 🎮**
+- Player will have a rocket using which he can fire any allien flights
+- There will multiple flights coming to ground
+- If any one flight touches ground then the game will be over
+- The number of flights the plyaer can destroy will be gained as points
+
+## **How to play? 🕹️**
+- Using right right and left arrow the rocket can be shifted to right and left respectively
+- Using up arrow the rocket wil fire
+
+## **Screenshots 📸**
+![Alien_Shooting_Game](https://github.com/sujanrupu/GameZone/assets/103595490/2ca6c8cd-c216-46da-8039-bd504ff53131)
+
+
+## **Working video 📹**
+
+
+https://github.com/sujanrupu/GameZone/assets/103595490/7a9320a8-3130-4c9a-b512-ffd182af9f21
---
+++ Games/Alien_Shooting_Game/script.js
@@ -0,0 +1,93 @@
+var jet = document.getElementById("jet");
+var board = document.getElementById("board");
+
+window.addEventListener("keydown", (e) => {
+  var left = parseInt(window.getComputedStyle(jet).getPropertyValue("left"));
+  if (e.key == "ArrowLeft" && left > 0) {
+    jet.style.left = left - 10 + "px";
+  }
+  //460  =>  board width - jet width
+  else if (e.key == "ArrowRight" && left <= 460) {
+    jet.style.left = left + 10 + "px";
+  }
+
+  if (e.key == "ArrowUp" || e.keyCode == 32) {
+    //32 is for space key
+    var bullet = document.createElement("div");
+    bullet.classList.add("bullets");
+    board.appendChild(bullet);
+
+    var movebullet = setInterval(() => {
+      var rocks = document.getElementsByClassName("rocks");
+
+      for (var i = 0; i < rocks.length; i++) {
+        var rock = rocks[i];
+        if (rock != undefined) {
+          var rockbound = rock.getBoundingClientRect();
+          var bulletbound = bullet.getBoundingClientRect();
+
+          //Condition to check whether the rock/alien and the bullet are at the same position..!
+          //If so,then we have to destroy that rock
+
+          if (
+            bulletbound.left >= rockbound.left &&
+            bulletbound.right <= rockbound.right &&
+            bulletbound.top <= rockbound.top &&
+            bulletbound.bottom <= rockbound.bottom
+          ) {
+            rock.parentElement.removeChild(rock); //Just removing that particular rock;
+            //Scoreboard
+            document.getElementById("points").innerHTML =
+              parseInt(document.getElementById("points").innerHTML) + 1;
+          }
+        }
+      }
+      var bulletbottom = parseInt(
+        window.getComputedStyle(bullet).getPropertyValue("bottom")
+      );
+
+      //Stops the bullet from moving outside the gamebox
+      if (bulletbottom >= 500) {
+        clearInterval(movebullet);
+      }
+
+      bullet.style.left = left + "px"; //bullet should always be placed at the top of my jet..!
+      bullet.style.bottom = bulletbottom + 3 + "px";
+    });
+  }
+});
+
+var generaterocks = setInterval(() => {
+  var rock = document.createElement("div");
+  rock.classList.add("rocks");
+  //Just getting the left of the rock to place it in random position...
+  var rockleft = parseInt(
+    window.getComputedStyle(rock).getPropertyValue("left")
+  );
+  //generate value between 0 to 450 where 450 => board width - rock width
+  rock.style.left = Math.floor(Math.random() * 450) + "px";
+
+  board.appendChild(rock);
+}, 1000);
+
+var moverocks = setInterval(() => {
+  var rocks = document.getElementsByClassName("rocks");
+
+  if (rocks != undefined) {
+    for (var i = 0; i < rocks.length; i++) {
+      //Now I have to increase the top of each rock,so that the rocks can move downwards..
+      var rock = rocks[i]; //getting each rock
+      var rocktop = parseInt(
+        window.getComputedStyle(rock).getPropertyValue("top")
+      );
+      //475 => boardheight - rockheight + 25
+      if (rocktop >= 475) {
+        alert("Game Over");
+        clearInterval(moverocks);
+        window.location.reload();
+      }
+
+      rock.style.top = rocktop + 25 + "px";
+    }
+  }
+}, 450);

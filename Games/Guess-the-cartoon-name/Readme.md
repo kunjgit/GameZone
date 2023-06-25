292971d1f342b058e25b082db3e691acc0de1a5e
shineishukla <shinei27shukla@gmail.com>
2023-06-25 22:02:18 +0530

    made changes

---
+++ Games/Guess-the-cartoon-name/Readme.md
@@ -0,0 +1,39 @@
+# Guess_the_cartoon_name
+
+---
+
+<br>
+
+# Description 📃
+Guess the Cartoon Name" is an interactive web application designed using HTML, CSS, and JavaScript. This engaging game challenges users to test their knowledge of popular cartoon names and allowing them to guess the correct answer.
+
+
+- 
+
+# Features
+1- Time Limit: The game incorporates a countdown timer to add excitement and challenge. At the start of each round, the timer begins counting down from a specified duration. If the player fails to guess the cartoon name within the time limit, the game moves on to the next round.
+
+2- Picture Change: Each round presents a cartoon image associated with a specific cartoon name. If the player is struggling to guess the answer, they can utilize the "Change Picture" feature. By clicking on a button or icon, the current cartoon image is replaced with a new one, providing an additional chance to identify the cartoon name.
+
+3- Correct Answer Reward: When the player correctly guesses the cartoon name, the game rewards them with points, displays a congratulatory message, and potentially triggers a celebratory animation. These positive reinforcements motivate players to continue playing and achieving higher scores.
+
+4- Restarting the Game: After completing all the rounds or at any point during the game, players can choose to start the game again. By clicking on a "Restart/Refresh" button, the game resets the score, round count, and timer, allowing players to enjoy the game from the beginning.
+- 
+<br>
+
+## **How to play? 🕹️**
+- Give the answer and fill the answer into the boxes
+- If answer is correct, score will increase by 1
+
+<br>
+
+## **Screenshots 📸**
+
+<br>
+<!-- add your screenshots like this -->
+<!-- ![image](url) -->
+
+<br>
+
+## **Working video 📹**
+<!-- add your working video over here -->
--- Games/Guess-the-cartoon-name/script.js
+++ Games/Guess-the-cartoon-name/script.js
@@ -1,81 +1,115 @@
-var characters = [
-    {name:"Shinchan", image:"https://pic-bstarstatic.akamaized.net/ugc/f0f768faa2d83bfef7dddd0300ba87cd.jpg@720w_405h_1e_1c_90q" },
-     {name:"Doraemon", image:"https://www.japantimes.co.jp/wp-content/uploads/2020/05/np_file_13590.jpeg"},
-     {name:"Pokemon", image:"https://img.lemde.fr/2022/12/22/5/0/1730/865/768/384/75/0/e968e4d_1671703423578-b5e.jpeg"}
-   ];
-   
-   var currentCharacterIndex = 0;
-   var timer;
-   var timeLimit = 30;
-   var score = 0;
-   
-   function startTimer() {
-     var timerElement = document.getElementById("timer");
-     timerElement.textContent = timeLimit;
-   
-     timer = setInterval(function() {
-       if (timeLimit <= 0) {
-         clearInterval(timer);
-         showNextCharacter();
-       }
-       else {
-         timeLimit--;
-         timerElement.textContent = timeLimit;
-       }
-     }, 1000);
-   }
-   
-   function showNextCharacter() {
-     var characterImage = document.getElementById("character-image");
-     var characterInput = document.getElementById("character-input");
-     var result = document.getElementById("result");
-   
-     var userInput = characterInput.value.trim().toLowerCase();
-     var currentCharacter = characters[currentCharacterIndex];
-     if (userInput === currentCharacter.name.toLowerCase()) {
-       score++;
-       result.textContent = "Correct!";
-     } else {
-       result.textContent = "Wrong!";
-     }
-   
-     characterInput.value = "";
-   
-     currentCharacterIndex++;
-     if (currentCharacterIndex >= characters.length) {
-       clearInterval(timer);
-       characterImage.style.display = "none";
-       characterInput.style.display = "none";
-       result.textContent = "Game Over! Score: " + score;
-       addToLeaderboard(score);
-     } else {
-       characterImage.src = characters[currentCharacterIndex].image;
-       startTimer();
-     }
-   }
-   
-   function changeImage() {
-     var characterImage = document.getElementById("character-image");
-     var randomIndex = Math.floor(Math.random() * characters.length);
-     characterImage.src = characters[randomIndex].image;
-   }
-   
-   function addToLeaderboard(score) {
-     var scoresList = document.getElementById("scores-list");
-     var scoreItem = document.createElement("li");
-     scoreItem.textContent = score;
-     scoresList.appendChild(scoreItem);
-   }
-   
-   window.addEventListener("load", function() {
-     var submitBtn = document.getElementById("submit-btn");
-     var changeImageBtn = document.getElementById("change-image-btn");
-     var shareBtn = document.getElementById("share-btn");
-     var challengeBtn = document.getElementById("challenge-btn");
-   
-     startTimer();
-   
-     submitBtn.addEventListener("click", showNextCharacter);
-     changeImageBtn.addEventListener("click", changeImage);
-   });
-   
+// Array of image names and corresponding answers
+const images = [
+    { name: "https://qph.cf2.quoracdn.net/main-qimg-1e393f372465529ab53d763dc3ceb214-pjlq", answer: "Shinchan" },
+    { name: "https://c4.wallpaperflare.com/wallpaper/447/181/100/cartoons-wallpaper-preview.jpg", answer: "Ben-10" },
+    { name: "https://www.finder.com.au/niche-builder/5f878e02db876.jpg", answer: "Dora the explorer" },
+    {name:"https://www.japancitytour.com/wp/wp-content/uploads/2021/03/doraemon.jpg", answer:"Doraemon"},
+    {name:"https://images.theconversation.com/files/210123/original/file-20180313-30983-1bk88tb.jpg?ixlib=rb-1.1.0&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip", answer:"Tom and jerry"},
+    {name:"https://i.guim.co.uk/img/media/66e444bff77d9c566e53c8da88591e4297df0896/120_0_1800_1080/master/1800.png?width=465&quality=85&dpr=1&s=none", answer:"Pokemon"},
+    {name:"https://www.animationxpress.com/wp-content/uploads/2021/09/AAAABe9UGuK5ypr_l6MWT2QflANEaMtgvNk_-2yl7LyoquFHhVvZTV7prNGZkYP4uKRCM8sn3CQcr_7lwflbZnp8yqSaToI8.jpg", answer:"Oggy and the coackreches"},
+    {name:"https://ichef.bbci.co.uk/images/ic/1200x675/p08rkqxl.jpg",answer:"Bob the builder"},
+    {name:"https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=face&w=510&h=255&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2012%2F10%2Fsofia-the-first-mother.jpg", answer:"Sofia the first"},
+    {name:"https://cdn.britannica.com/08/190808-050-CB26C47B/The-Powerpuff-Girls-Bubbles-Blossom-Buttercup.jpg", answer:"The Powerpuff Girls"},
+    {name:"https://w7.pngwing.com/pngs/473/387/png-transparent-a-world-of-winnie-the-pooh-winnie-the-pooh-piglet-eeyore-winnie-the-pooh-and-friends-winnie-the-pooh-characters-computer-wallpaper-cartoons-cartoon-thumbnail.png", answer:"Winnie The Pooh"},
+    {name:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/3f3dba4f477f5d295cb55f1915c9623830875dcd8fe8a493a488912a0954b0e0._RI_TTW_SX720_FMjpg_.png", answer:"Mr Bean"},
+    {name:"https://is1-ssl.mzstatic.com/image/thumb/Features113/v4/50/ad/5e/50ad5efd-12dd-117d-141f-519fb50cd238/mzl.ydwoaeoz.png/1200x675mf.jpg", anser:"Mickey Mouse"}
+  ];
+  
+  let currentImageIndex = 0;
+  let score = 0;
+  const scoreList = [];
+  
+  // Function to update the score display
+  function updateScore() {
+    document.getElementById("scoreValue").textContent = score;
+  }
+  
+  // Function to display the next image
+  function showNextImage() {
+    if (currentImageIndex < images.length) {
+      const image = document.getElementById("image");
+      image.src = images[currentImageIndex].name;
+      image.alt = "Guess the Name";
+      document.getElementById("guessInput").value = "";
+      document.getElementById("message").textContent = "";
+    } else {
+      endGame();
+    }
+  }
+  
+  // Function to check the guess and provide feedback
+  function checkGuess() {
+    const guessInput = document.getElementById("guessInput");
+    const guess = guessInput.value.toLowerCase();
+    const answer = images[currentImageIndex].answer.toLowerCase();
+  
+    if (guess === answer) {
+      score++;
+      updateScore();
+      document.getElementById("message").textContent = "Correct!";
+    } else {
+      document.getElementById("message").textContent = "Wrong! Try again.";
+    }
+  
+    currentImageIndex++;
+    showNextImage();
+  }
+  
+  // Function to change the picture manually
+  function changePicture() {
+    if (currentImageIndex < images.length) {
+      currentImageIndex++;
+      showNextImage();
+    } else {
+      endGame();
+    }
+  }
+  
+  // Function to start the game over
+  function startOver() {
+    currentImageIndex = 0;
+    score = 0;
+    scoreList.length = 0;
+    updateScore();
+    document.getElementById("message").textContent = "";
+    document.getElementById("scoreItems").innerHTML = "";
+    showNextImage();
+  }
+  
+  // Function to end the game
+  function endGame() {
+    const guessInput = document.getElementById("guessInput");
+    const submitBtn = document.getElementById("submitBtn");
+    const changePictureBtn = document.getElementById("changePictureBtn");
+  
+    guessInput.disabled = true;
+    submitBtn.disabled = true;
+    changePictureBtn.disabled = true;
+    document.getElementById("message").textContent = "Game Over!";
+    displayScoreList();
+  }
+  
+  // Function to display the score list
+  function displayScoreList() {
+    const scoreItems = document.getElementById("scoreItems");
+    scoreItems.innerHTML = "";
+  
+    scoreList.forEach((score, index) => {
+      const scoreItem = document.createElement("li");
+      scoreItem.textContent = `Game ${index + 1}: ${score}`;
+      scoreItems.appendChild(scoreItem);
+    });
+  }
+  
+  // Event listener for the submit button
+  document.getElementById("submitBtn").addEventListener("click", checkGuess);
+  
+  // Event listener for the change picture button
+  document.getElementById("changePictureBtn").addEventListener("click", changePicture);
+  
+  // Event listener for the start over button
+  document.getElementById("startOverBtn").addEventListener("click", startOver);
+  
+  // Start the game
+  showNextImage();
+  

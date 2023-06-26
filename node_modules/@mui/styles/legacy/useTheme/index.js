78156f8bb6b044436d3c5c24ad7c896a1c9ba42d
linga66 <lingasreeja86@gmail.com>
2023-06-26 19:51:22 +0530

    updated

--- node_modules/@mui/styles/legacy/useTheme/index.js
+++
@@ -1,6 +0,0 @@
-import usePrivateTheme from '@mui/private-theming/useTheme';
-export default function useTheme() {
-  var _privateTheme$$$mater;
-  var privateTheme = usePrivateTheme();
-  return (_privateTheme$$$mater = privateTheme == null ? void 0 : privateTheme.$$material) != null ? _privateTheme$$$mater : privateTheme;
-}
--- node_modules/@mui/styles/useTheme/index.js
+++
@@ -1,6 +0,0 @@
-import usePrivateTheme from '@mui/private-theming/useTheme';
-export default function useTheme() {
-  var _privateTheme$$$mater;
-  const privateTheme = usePrivateTheme();
-  return (_privateTheme$$$mater = privateTheme == null ? void 0 : privateTheme.$$material) != null ? _privateTheme$$$mater : privateTheme;
-}

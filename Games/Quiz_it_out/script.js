f7adbaf1182f56ecfcd77a7e476af64cbbf87734
Sabeena Viklar <sabeenaviklar@Sabeenas-MacBook-Air.local>
2023-07-19 23:15:02 +0530

    correct and incorrect color added

--- Games/Quiz_it_out/script.js
+++ Games/Quiz_it_out/script.js
@@ -71,6 +71,9 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
         button.addEventListener("click", selectAnswer);
     })
 }
@@ -79,6 +82,16 @@ function resetState(){
     nextButton.style.display = "none";
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === 'true';
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+    }else{
+        selectBtn.classList.add("incorrect");
     }
 }
 

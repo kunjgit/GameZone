028035c3e012338ec1954d97f5e5b898aad1866a
Kris0011 <krishp759@gmail.com>
2024-01-21 18:21:49 +0530

    Fixed gamezone title

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,8 +14,6 @@ const generateLiTags = (gamesData) => {
               <figure class="project-img">
                 <div class="project-item-icon-box">
                   <img id="joystick" src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Activities/Video%20Game.png" alt="Eye" width="3" />
-                  <p class="gameTitle">${gameTitle}</p>
-                  <p class="gameTitle">${gameTitle}</p>
                 </div>
                 <img src="./assets/images/${thumbnailUrl}" alt="${gameTitle}" loading="lazy">
               </figure>

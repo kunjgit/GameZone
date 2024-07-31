fa19ed7f362cbd2c40d2db4eb00c32d82cd1e604
Aditi <aditijain132005@gmail.com>
2024-07-31 12:19:49 +0530

    Add files via upload
    Added game

---
+++ Games/Gem_Cruncher/src/components/_LevelTarget.svelte
@@ -0,0 +1,45 @@
+<style>
+  .level-target {
+    display: flex;
+    font-size: 18px;
+    line-height: normal;
+    justify-content: center;
+    padding: 6px;
+  }
+
+  .objective {
+    margin-left: 30px;
+  }
+
+  .objective:first-child {
+    margin-left: 0;
+  }
+
+  .objective img {
+    display: block;
+    width: 30px;
+    height: 30px;
+    margin-bottom: 2px;
+  }
+
+  .objective span {
+    display: block;
+    font-size: 14px;
+    letter-spacing: 1px;
+  }
+</style>
+
+<div class="level-target">
+  <div class="objective">
+    <img src="/assets/tiles/red.png" alt="Red Tiles" />
+    <span>5/15</span>
+  </div>
+  <div class="objective">
+    <img src="/assets/tiles/blue.png" alt="Red Tiles" />
+    <span>5/15</span>
+  </div>
+  <div class="objective">
+    <img src="/assets/tiles/purple.png" alt="Red Tiles" />
+    <span>5/15</span>
+  </div>
+</div>

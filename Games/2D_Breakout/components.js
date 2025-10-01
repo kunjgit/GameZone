5ef3f85cbfe588c46ca1ea88cf605b57dac9ad39
Shivani Pillai <shivani.pillaiedu0208@gmail.com>
2025-10-01 13:07:33 +0530

    Fix: corrected Audio object initialization for sound loading

--- Games/2D_Breakout/components.js
+++ Games/2D_Breakout/components.js
@@ -13,27 +13,16 @@ LIFE_IMG.src = "img/life.png";
 const SCORE_IMG = new Image();
 SCORE_IMG.src = "img/score.png";
 
-
 /////// END LOAD IMAGES ////////
 
 // ************************ //
 
 /////// LOAD SOUNDS ////////
 
-const WALL_HIT = new Audio();
-WALL_HIT.src = "sounds/wall.mp3";
-
-const LIFE_LOST = new Audio();
-LIFE_LOST.src = "sounds/life_lost.mp3";
-
-const PADDLE_HIT = new Audio();
-PADDLE_HIT.src = "sounds/paddle_hit.mp3";
-
-const WIN = new Audio();
-WIN.src = "sounds/win.mp3";
-
-const BRICK_HIT = new Audio();
-BRICK_HIT.src = "sounds/brick_hit.mp3";
-
+const WALL_HIT = new Audio("sounds/wall.mp3");
+const LIFE_LOST = new Audio("sounds/life_lost.mp3");
+const PADDLE_HIT = new Audio("sounds/paddle_hit.mp3");
+const WIN = new Audio("sounds/win.mp3");
+const BRICK_HIT = new Audio("sounds/brick_hit.mp3");
 
 /////// END LOAD SOUNDS ////////
